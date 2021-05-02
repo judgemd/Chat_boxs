bcc60b06cf44ca6d80e1bb57dffea67ae1233c45
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-05-02 13:38:58 +0300

    Create connection.php

---
+++ system/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$database = "chat";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+	if(!$conn) {
+		die("Failed" . mysqli_connect_error());
+	}
+?>
