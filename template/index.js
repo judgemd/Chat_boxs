d515e20b11786b74d4f142fee35b3a8103b441bc
Ashraf Mammadov <77106029+judgemd@users.noreply.github.com>
2021-05-02 13:38:33 +0400

    Update index.js

---
+++ template/index.js
@@ -0,0 +1,8 @@
+function register () {
+  document.getElementById('containerid').style.display = "none";
+  document.getElementById('containerid2').style.display = "block";
+}
+
+function closebtn() {
+  document.getElementById("containerid2").style.display = "none"
+}
