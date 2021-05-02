25bdc2b9576c834391063753ad39eee42cbfab02
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-05-02 13:04:44 +0300

    Revert "Delete unnecessary files"
    This reverts commit af91dc58772e1b32ebeb69afbe960c237c4fca7f.

---
+++ chatOOP2.php
@@ -0,0 +1,5 @@
+<?php?>
+<div class="message me">
+    <div class="bubble">Hi, What's up?</div>
+    <div class="time">1 minute ago</div>
+</div>
---
+++ textOOP.php
@@ -0,0 +1,11 @@
+<?PHP?>
+<li>
+    <a href = "#">
+    <img src = "images/ashraf.JPG">
+    <div class="contact">
+        <div class="name">Ashraf Mammadov</div>
+        <div class="message">Hi, This is a test message</div>
+    </div>
+    <div class="notification"></div>
+</a>
+</li>
