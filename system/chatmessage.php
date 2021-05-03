1aa836c4691d6cc31c9a193b9898e64de9871c1f
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-05-03 16:16:55 +0300

    Bug fix

--- system/chatmessage.php
+++ system/chatmessage.php
@@ -2,10 +2,10 @@
 $text = $_POST["text"];
 $username = $_SESSION["username"];
 
-$sql = "INSERT INTO messages(username, text) VALUES('$text','$username')");
+$sql = "INSERT INTO messages(username, text) VALUES ('$text','$username')";
 
   if(mysqli_query($conn, $sql)) {
-    header("Location: chat/chat.php");
+    header("Location: ../chat/chat.php");
   } else {
     die(mysqli_error($conn));
   }
