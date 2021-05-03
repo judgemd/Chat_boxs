f788800b0361660f83d924f05cf531f8bc62d378
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-05-03 12:04:12 +0300

    Create chatmessage.php

---
+++ system/chatmessage.php
@@ -0,0 +1,12 @@
+<?PHP
+$text = $_POST["text"];
+$username = $_SESSION["username"];
+
+$sql = "INSERT INTO messages(username, text) VALUES('$text','$username')");
+
+  if(mysqli_query($conn, $sql)) {
+    header("Location: chat/chat.php");
+  } else {
+    die(mysqli_error($conn));
+  }
+?>
